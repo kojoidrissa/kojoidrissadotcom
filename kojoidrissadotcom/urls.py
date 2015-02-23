2e8594b2266f3434b799e0b6ce71eaf1e550d9b6
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-02-23 15:51:17 -0600

    setup index view url for polls

--- kojoidrissadotcom/urls.py
+++ kojoidrissadotcom/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'kojoidrissadotcom.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+    
