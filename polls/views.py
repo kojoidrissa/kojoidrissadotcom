b151312b783aa818b477a8eea2f62f132f5c848b
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-03-31 18:11:09 -0500

    updated voting error message in polls/views.py; removed blank line from urls.py

--- polls/views.py
+++ polls/views.py
@@ -27,7 +27,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': p,
-            'error_message': "You didn't select a choice.",
+            'error_message': "You didn't select a choice. You gotta play to win!",
         })
     else:
         selected_choice.votes += 1
