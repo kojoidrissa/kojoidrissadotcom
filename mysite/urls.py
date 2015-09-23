9b7f148aa24feab1479a3ad7819c04344b0fa122
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-09-22 20:11:50 -0500

    Made changes for login.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
+
 from django.contrib import admin
+admin.autodiscover()
+
 
 urlpatterns = [
     # Examples:
@@ -7,5 +10,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'', include('blog.urls')),
 ]
